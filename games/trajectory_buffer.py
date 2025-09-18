add advantage normalization, add learning rate annealing and fix trajectory buffer bug for ppo

games/trajectory_buffer.py
@@ -14,8 +14,9 @@ class TrajectoryBuffer:
         self.length = length
         self.env = env
         self.num_env = env.num_envs
-        self.indicies = np.arange(self.length * self.num_env)
-        self.mb_length = self.length * self.num_env // num_minibatches
+        self.batch_size = self.length * self.num_env
+        self.indicies = np.arange(self.batch_size)
+        self.mb_length = self.batch_size // num_minibatches
         self.observations = torch.zeros((length, self.num_env) + env.single_observation_space.shape).to(device)
         self.actions = torch.zeros((length, self.num_env)).to(device)
         self.rewards = torch.zeros((length, self.num_env)).to(device)
@@ -77,7 +78,7 @@ class TrajectoryBuffer:
         b_values = self.values.reshape(-1)
         # using numpy to randomly shuffle batch indecies
         np.random.shuffle(self.indicies)
-        for start in range(0, self.length, self.mb_length):
+        for start in range(0, self.batch_size, self.mb_length):
             end = start + self.mb_length
             mb_ind = self.indicies[start:end]
             yield b_obs[mb_ind], b_logprobs[mb_ind], b_actions[mb_ind], b_advantages[mb_ind], b_returns[mb_ind], b_values[mb_ind]

models/model.py
@@ -87,7 +87,7 @@ class DeepAgent(BaseAgent):
     def save_agent(self, path: str, networks: Dict[str, nn.Module], 
                    optimizers: Dict[str, torch.optim.Optimizer], 
                    additional_data: Optional[Dict[str, Any]] = None) -> None:
-        """Common save pattern for all agents. This method is intended to be run inside agent.save() method"""
+        """This method is intended to be run inside agent.save() method. Common save pattern for all agents. """
         checkpoint_path = self._save_checkpoint(
             self.episode, networks, optimizers, additional_data
         )

models/ppo.py
@@ -2,7 +2,7 @@ from models.vectorized_trainer import VectorizedTrainer
 from games.trajectory_buffer import TrajectoryBuffer
 from .network import FCNetwork
 
-from typing import Dict, Any, Tuple
+from typing import Dict, Any, Tuple, List
 import numpy as np
 import os
 import torch
@@ -76,6 +76,15 @@ class PPOAgent(VectorizedTrainer):
         self.network = PPOSeparateFC(self.env).to(self.device)
         lr = self.train_cfg.get("learning_rate", 3e-4)
         self.optimizer = self._setup_optimizer(self.network, lr)
+        # temporary for comparison with cleanrl implementation -----
+        layout = {
+            "Returns": {
+                "return_comparison": ["Multiline", ["global_step/reward", "charts/episodic_return"]],
+            }
+        }
+        self.writer.add_custom_scalars(layout)
+        # to be removed -----
+
 
     
     def get_algorithm_name(self):
@@ -85,12 +94,16 @@ class PPOAgent(VectorizedTrainer):
         next_obs, _ = self.env.reset()
         next_done = np.zeros(self.num_env)
         minibatch_number = self.train_cfg.get("mb_num", 4)
-        self.trajectory_buffer = TrajectoryBuffer(self.num_steps, self.env, minibatch_number, self.device)
         gamma = self.train_cfg.get("gamma", 0.99)
         gae_lambda = self.train_cfg.get("lambda", 0.95)
+        lr = self.train_cfg.get("learning_rate", 3e-4)
+        lr_anneal = self.train_cfg.get("lr_anneal", True)
+        self.trajectory_buffer = TrajectoryBuffer(self.num_steps, self.env, minibatch_number, self.device)
 
         for update in range(self.num_episodes):
-            # here add learning rate annealing
+            # learning rate annealing
+            if lr_anneal:
+                self.anneal_lr(update, self.num_episodes, lr)
             for step in range(self.num_steps):
                 # s_t = s_t+1
                 obs = next_obs
@@ -125,6 +138,11 @@ class PPOAgent(VectorizedTrainer):
         clip_coef = self.train_cfg.get("clip_coef", 0.2)
         ent_coef = self.train_cfg.get("ent_coef", 0.01)
         vf_coef = self.train_cfg.get("vf_coef", 0.5)
+        adv_norm = self.train_cfg.get("adv_norm", True)
+
+        losses = []
+        # the fraction of training data that triggered the clipped objective
+        clipfracts = []
 
         for step in range(lr_steps):
             training_data = self.trajectory_buffer.get_batches()
@@ -134,22 +152,33 @@ class PPOAgent(VectorizedTrainer):
                 logratio = new_logprob - b_logprobs
                 ratio = logratio.exp()
                 
-                # here add advantage normalization
+                with torch.no_grad():
+                    clipfracts+= [((ratio - 1.0).abs() > clip_coef).float().mean().item()]
 
+                # advantage normalization
+                if adv_norm:
+                    b_advantages = self.normalize_adv(b_advantages)
                 # VALUE LOSS
                 v_loss = 0.5 * ((new_value - b_returns) ** 2).mean()
 
                 # POLICY LOSS
                 p_loss_unclipped = -b_advantages * ratio
                 p_loss_clipped = -b_advantages * torch.clamp(ratio, 1 - clip_coef, 1 + clip_coef)
-                p_loss = torch.max(p_loss_unclipped, p_loss_clipped,).mean()
+                p_loss = torch.max(p_loss_unclipped, p_loss_clipped).mean()
 
                 entropy_loss = entropy.mean()
                 loss = p_loss - ent_coef * entropy_loss + vf_coef * v_loss
                 self.optimizer.zero_grad()
+                # calcualte gradients
                 loss.backward()
+                # clipping gradients
+                self._apply_gradient_clipping(self.network)
+
                 self.optimizer.step()
-                self._log_batch_data(self.episode, loss)
+                # record loss value
+                losses.append(loss.item())
+        self._log_batch_data(self.episode, losses)
+        self.log_debug_data(self.optimizer, self.steps_done, v_loss.item(), entropy_loss.item(), p_loss.item(), clipfracts)
 
     def act_and_value(self, state, action: torch.Tensor=None) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
         """select action using current policy, compute the value of the state, compute the entrop
@@ -197,3 +226,20 @@ class PPOAgent(VectorizedTrainer):
 
     def evaluate(self):
         pass
+
+    def normalize_adv(self, adv_tensor: torch.Tensor) -> torch.Tensor:
+        """ normalize advantages """
+        return (adv_tensor - adv_tensor.mean()) / (adv_tensor.std() + 1e-8)
+    
+    def anneal_lr(self, current_step, total_steps, original_lr):
+        frac = 1 - current_step / total_steps
+        new_lr = original_lr * frac
+        self.optimizer.param_groups[0]["lr"] = new_lr
+
+    def log_debug_data(self, optimizer, global_step, v_loss: float, pg_loss: float, entropy_loss: float, clipfracs: List[float]):
+        self.writer.add_scalar("charts/learning_rate", optimizer.param_groups[0]["lr"], global_step)
+        self.writer.add_scalar("losses/value_loss", v_loss, global_step)
+        self.writer.add_scalar("losses/policy_loss", pg_loss, global_step)
+        self.writer.add_scalar("losses/entropy", entropy_loss, global_step)
+        self.writer.add_scalar("losses/clipfrac", np.mean(clipfracs), global_step)
+

models/vectorized_trainer.py
@@ -20,6 +20,11 @@ class VectorizedTrainer(DeepAgent):
     def training_loop(self):
         pass
 
+    @abstractmethod
+    def log_debug_data(self):
+        """log episode specific debug data """
+        pass
+
     def train(self) -> None:
         """training in a vectorized environment."""
         # setup variables needed for training
@@ -39,7 +44,7 @@ class VectorizedTrainer(DeepAgent):
 
         self.cleanup()
 
-    def _log_episode_data(self, ep, episode_reward, episode_length):
+    def _log_episode_data(self, ep, global_step, episode_reward, episode_length):
         """log the episode data to wandb"""
         stats_window = int(self.train_cfg.get("stats_window", 100))
         self.recent_rewards.append(episode_reward)
@@ -55,13 +60,15 @@ class VectorizedTrainer(DeepAgent):
         if hasattr(self, 'writer'):
             self.writer.add_scalar("episode/reward", episode_reward, ep)
             self.writer.add_scalar("episode/steps", episode_length, ep)
+            self.writer.add_scalar("global_step/reward", episode_reward, global_step)
+            self.writer.add_scalar("global_step/steps", episode_reward, global_step)
             if hasattr(self, 'rolling_avg'):
                 self.writer.add_scalar("episode/rolling_avg", rolling_avg, ep)
                 self.writer.add_scalar("episode/rolling_std", rolling_std, ep)
 
-    def _log_batch_data(self, ep, loss):
+    def _log_batch_data(self, ep, loss: List[float]):
             if hasattr(self, 'writer'):
-                self.writer.add_scalar("batch/loss", loss, ep)
+                self.writer.add_scalar("episode/avg_loss", np.mean(loss), ep)
 
 
 
@@ -108,11 +115,11 @@ class VectorizedTrainer(DeepAgent):
             step = self.steps_done
             for reward, length in zip(rewards, lengths):
                 self.episode += 1
-                self._print_episode_progress(self.episode, reward, length)
-                self._log_episode_data(self.episode, reward, length)
+                self._print_episode_progress(self.steps_done, reward, length)
+                self._log_episode_data(self.episode, self.steps_done, reward, length)
 
-    def _print_episode_progress(self, episode: int, reward: float, length: int):
+    def _print_episode_progress(self, global_step: int, reward: float, length: int):
         """Print episode progress."""
-        print(f"Ep {episode:4d} | R {reward:8.2f} | steps {length:5d}")
+        print(f"Ep {global_step:4d} | R {reward:8.2f} | steps {length:5d}")
 
 